51399b4ab7ed146c03127e427d57c4d5ac416f58
Taku <fk.53rk2@icloud.com>
2024-10-08 12:04:51 +0900

    以前削除してしまったファイルを追加した

---
+++ my-app/src/Components/ReservBox/ReservCustomhooks/SelectTimes.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+export const SelectTimes = () => {
+    const [times, setTimes] = useState("")
+    const handleTimes = (e) => {
+        setTimes(e.target.value);
+    }
+    return (
+        <form>
+           <SInput type="text" value={times} onChange={handleTimes} /> 
+        </form>
+    )
+}
+const SInput = styled.input`
+    transform: translateX(10px);
+    margin: 5px;
+    padding: 3px;
+`
